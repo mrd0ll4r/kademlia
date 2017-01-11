632e092acb9855b8f6a82046b8d91b6cdc1e5394
Leo Balduf <balduf@hm.edu>
2017-01-11 20:56:12 +0100

    implement node lookup, bootstrap, findNode, store

--- cmd/kademlia/main.go
+++ cmd/kademlia/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"strings"
 
 	log "github.com/Sirupsen/logrus"
 
@@ -9,20 +10,26 @@ import (
 )
 
 var addr string
+var bootstrapNodes string
 
 func init() {
 	flag.StringVar(&addr, "a", "localhost:1337", "address to bind to")
+	flag.StringVar(&bootstrapNodes, "bootstrap", "localhost:1338,localhost:1339", "comma-separated addresses of bootstrap nodes")
 }
 
 func main() {
 	flag.Parse()
-	node, err := kademlia.NewNode(addr)
+	node, err := kademlia.NewNode(kademlia.NodeConfig{
+		ID:             kademlia.NewNodeID(),
+		Addr:           addr,
+		BootstrapNodes: strings.Split(bootstrapNodes, ","),
+	})
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer node.Stop()
 
-	log.Println("Own ID:", node.ID())
+	log.Printf("Own ID: %x", node.ID())
 
 	// We now have a node and can use it.
 }
--- node_test.go
+++ node_test.go
@@ -4,40 +4,36 @@ import (
 	"testing"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNode_Ping(t *testing.T) {
 	log.SetLevel(log.DebugLevel)
-	id1 := NodeIDFromBytes([]byte("aaaaaaaaaaaaaaaaaaaa"))
-	addr1 := "localhost:1337"
-
-	id2 := NodeIDFromBytes([]byte("bbbbbbbbbbbbbbbbbbbb"))
-	addr2 := "localhost:1338"
-
-	node1, err := newNodeWithFixedID(addr1, id1)
-	if err != nil {
-		t.Error(err)
+	cfg1 := NodeConfig{
+		ID:             NodeIDFromBytes([]byte("aaaaaaaaaaaaaaaaaaaa")),
+		Addr:           "localhost:1337",
+		BootstrapNodes: []string{"localhost:1338"},
 	}
+	cfg2 := NodeConfig{
+		ID:             NodeIDFromBytes([]byte("bbbbbbbbbbbbbbbbbbbb")),
+		Addr:           "localhost:1338",
+		BootstrapNodes: []string{"localhost:1337"},
+	}
+
+	node1, err := NewNode(cfg1)
+	require.Nil(t, err)
 	defer node1.Stop()
 
-	node2, err := newNodeWithFixedID(addr2, id2)
-	if err != nil {
-		t.Error(err)
-	}
+	node2, err := NewNode(cfg2)
+	require.Nil(t, err)
 	defer node2.Stop()
 
-	err = node1.Connect(id2, addr2)
-	if err != nil {
-		t.Error(err)
-	}
+	err = node1.Connect(cfg2.ID, cfg2.Addr)
+	require.Nil(t, err)
 
-	err = node1.Ping(id2)
-	if err != nil {
-		t.Error(err)
-	}
+	err = node1.Ping(cfg2.ID)
+	require.Nil(t, err)
 
-	err = node2.Ping(id1)
-	if err != nil {
-		t.Error(err)
-	}
+	err = node2.Ping(cfg1.ID)
+	require.Nil(t, err)
 }
